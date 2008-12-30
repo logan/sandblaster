a44c44a138bb6a92917906f214b4e1d76f96346c
logan.hanks <none@none>
2008-12-30 23:16:21 +0000

    Add a crude app icon and make the layout scale better.
    --HG--
    extra : convert_revision : svn%3Ac9028f1e-d6a8-11dd-801b-4930de48a34e/trunk%406

--- src/com/loganh/sandblaster/R.java
+++ src/com/loganh/sandblaster/R.java
@@ -10,17 +10,20 @@ package com.loganh.sandblaster;
 public final class R {
     public static final class attr {
     }
+    public static final class drawable {
+        public static final int app_sandblaster=0x7f020000;
+    }
     public static final class id {
-        public static final int palette=0x7f040001;
-        public static final int sand=0x7f040000;
+        public static final int palette=0x7f050001;
+        public static final int sand=0x7f050000;
     }
     public static final class layout {
-        public static final int sand=0x7f020000;
+        public static final int sand=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
-        public static final int menu_bigger=0x7f030002;
-        public static final int menu_clear=0x7f030001;
-        public static final int menu_smaller=0x7f030003;
+        public static final int app_name=0x7f040000;
+        public static final int menu_bigger=0x7f040002;
+        public static final int menu_clear=0x7f040001;
+        public static final int menu_smaller=0x7f040003;
     }
 }
