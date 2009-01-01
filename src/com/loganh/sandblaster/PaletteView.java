44ad04e8d40336750d139d66ec1a6cf1fc3b8828
logan.hanks <none@none>
2009-01-01 22:34:04 +0000

    Add labels to palette items.
    --HG--
    extra : convert_revision : svn%3Ac9028f1e-d6a8-11dd-801b-4930de48a34e/trunk%4029

--- src/com/loganh/sandblaster/PaletteView.java
+++ src/com/loganh/sandblaster/PaletteView.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -16,9 +17,13 @@ public class PaletteView extends View {
   public static Element[] ELEMENTS = EnumSet.range(Element.WALL, Element.FIRE).toArray(new Element[]{});
 
   public int selected;
+  private String[] labels;
+  private Point[] labelPositions;
 
   public PaletteView(Context context, AttributeSet attrs) {
     super(context, attrs);
+    labels = new String[ELEMENTS.length];
+    labelPositions = new Point[ELEMENTS.length];
   }
 
   @Override
@@ -39,8 +44,24 @@ public class PaletteView extends View {
       paint.setStyle(Paint.Style.STROKE);
       paint.setStrokeWidth(2);
       canvas.drawRect(rect, paint);
+      drawLabel(offset, canvas, rect, element);
       offset++;
     }
+  }
+
+  private void drawLabel(int offset, Canvas canvas, Rect button, Element element) {
+    Paint paint = new Paint();
+    if (labels[offset] == null) {
+      String capName = element.toString();
+      String label = new StringBuffer().append(capName.charAt(0)).append(capName.substring(1).toLowerCase()).toString();
+      Rect bounds = new Rect();
+      paint.getTextBounds(label, 0, label.length(), bounds);
+      int left = Math.max(button.left, button.centerX() - bounds.centerX());
+      int top = Math.max(button.top, button.centerY() - bounds.centerY());
+      labels[offset] = label;
+      labelPositions[offset] = new Point(left, top);
+    }
+    canvas.drawText(labels[offset], labelPositions[offset].x, labelPositions[offset].y, paint);
   }
 
   @Override
